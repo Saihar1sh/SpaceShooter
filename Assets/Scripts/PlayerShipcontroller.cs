c5447964b915ea353b31db2552a326835d370f9b
Har15h <saiharishmanne@gmail.com>
2021-07-29 02:12:58 +0530

    Player Movement implemented and build platform changed to Android.

---
+++ Assets/Scripts/PlayerShipcontroller.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+public class PlayerShipcontroller
+{
+    public PlayerShipcontroller(PlayerShipView playerShipView)
+    {
+        playerShipView.GetPlayerController(this);
+    }
+
+    public PlayerShipcontroller(PlayerShipView playerShipPrefab, PlayerShipModel playerShipModel, Vector3 pos, Quaternion quaternion)
+    {
+        PlayerShipView = GameObject.Instantiate<PlayerShipView>(playerShipPrefab, pos, quaternion);
+    }
+
+    public void ShipMovement(Vector3 movementInput, Rigidbody shipRb, float mvtSpeed)
+    {
+        //Debug.Log("horizontal: " + horizontal + " vertical:" + vertical);
+        shipRb.MovePosition(shipRb.position + movementInput * mvtSpeed * Time.deltaTime);
+        //Vector3 rotation = movementInput * rotatingSpeed;
+        //shipRb.transform.rotation = Quaternion.LookRotation(rotation);
+    }
+
+
+
+    public PlayerShipView PlayerShipView { get; }
+
+}
---
+++ Assets/Scripts/ShipService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipService : MonoSingletonGeneric<ShipService>
+{
+    public PlayerShipModel shipModel;
+    public PlayerShipcontroller shipcontroller;
+    public PlayerShipView playerShipView;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreatePlayerShip();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public PlayerShipcontroller CreatePlayerShip()
+    {
+        Vector3 randomSpawnPos = Vector3.zero; //new Vector3(UnityEngine.Random.Range(-42, 43), 0, UnityEngine.Random.Range(39, -43));
+        PlayerShipcontroller playerShipcontroller = new PlayerShipcontroller(playerShipView, shipModel, randomSpawnPos, Quaternion.Euler(0, 90, 0));
+        return playerShipcontroller;
+    }
+}
